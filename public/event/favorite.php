eaf94daa041274cf665d41d969f1c884b9b6045a
Matthew Cifelli <matthewcifelli@gmail.com>
2016-04-02 15:48:36 -0400

    Added request for favoriting an event.

---
+++ public/event/favorite.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once(__DIR__ . '/../../config.php');
+require_once(__DIR__ . '/../../src/Router.php');
+require_once(__DIR__ . '/../../src/Response.php');
+require_once(__DIR__ . '/../../src/HTTPException.php');
+
+$router = new Router();
+$router->on('post', function ($request, $services) {
+	session_start();
+	$stmt = $services['pdo']->prepare('SELECT `ownerID` FROM `Session` WHERE `id` = :sessionID');
+	$stmt->bindValue('sessionID', session_id(), PDO::PARAM_STR);
+	$stmt->execute();
+	$currentUserID = $stmt->fetchColumn(0);
+	if ($currentUserID === false) {
+		throw new UnauthorizedException('You must be logged in to favorite an event.');
+	}
+
+
+	$request->params->mustHave('eventID');
+	$stmt = $services['pdo']->prepare('
+			INSERT IGNORE INTO `EventFavorites` (
+				`eventID`,
+				`userID`
+			) VALUES (
+				:eventID,
+				:userID
+			)
+	');
+	$stmt->bindValue('eventID', $request->params['eventID'], PDO::PARAM_INT);
+	$stmt->bindValue('userID',  $currentUserID,              PDO::PARAM_INT);
+	try {
+		$stmt->execute();
+	} catch (PDOException $e) {
+		if (-1 !== strpos($e->getMessage(), 'Integrity constraint violation')) {
+			throw new ObjectNotFoundException('Event does not exist.');
+		}
+	}
+	if ($stmt->rowCount() < 1) {
+		return new Response('', 304);
+	}
+});
+$router->route();
